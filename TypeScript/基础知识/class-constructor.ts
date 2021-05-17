c74cdeb2ec3c21efa5387ca4e07a2b04f39253ba
henansong <github@nansenho.com>
2021-05-17 11:13:36 +0800

    .

--- TypeScript/基础知识/class-constructor.ts
+++ TypeScript/基础知识/class-constructor.ts
@@ -7,3 +7,23 @@ class A{
 }
 const d = new A('x')
 console.log(d.b)
+
+
+// 当 b 和 c 相同时，即
+class A{
+    b: string;
+    constructor(b:string){
+        this.b = b
+    }
+}
+const c = new A('x')
+console.log(c.b)
+
+//可以简化成下面这种形式
+class A{
+    constructor(public b: string){
+        this.b = b
+    }
+}
+const c = new A('x')
+console.log(c.b)
