12c35af9e49db2018e79afdc5c08170e4c485dee
henansong <github@nansenho.com>
2021-05-21 11:41:56 +0800

    '根据url返回不同的文件-2'

--- Nodejs/node-server/url-response/index.ts
+++ Nodejs/node-server/url-response/index.ts
@@ -1,4 +1,4 @@
-import * as http from 'http';
+﻿import * as http from 'http';
 import {IncomingMessage, ServerResponse} from 'http';
 import * as fs from 'fs'; // TypeScript 用 * 号来引入，这样更加严谨
 import * as p from 'path';
@@ -13,7 +13,7 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
         // 这些静态文件存放在根目录下的 public/static 目录里
         // 这些静态文件都是运行在浏览器里，而不是 node 里面的
         case '/index.html':
-            response.setHeader('Content-Type', 'text/html; charset=utf-8')
+            response.setHeader('Content-Type', 'text/html; charset=utf-8');
             fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
                 // 读 public 目录下的 index.html 静态文件
                 if (error) throw error; // 看有没有错误
@@ -22,7 +22,7 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
             });
             break;
         case '/style.css':
-            response.setHeader('Content-Type', 'text/css; charset=utf-8')
+            response.setHeader('Content-Type', 'text/css; charset=utf-8');
             fs.readFile(p.resolve(publicDir, 'style.css'), (error, data) => {
                 // 读 public 目录下的 index.html 静态文件
                 if (error) throw error; // 看有没有错误
@@ -31,7 +31,7 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
             });
             break;
         case '/main.js':
-            response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
+            response.setHeader('Content-Type', 'text/javascript; charset=utf-8');
             fs.readFile(p.resolve(publicDir, 'main.js'), (error, data) => {
                 // 读 public 目录下的 index.html 静态文件
                 if (error) throw error; // 看有没有错误
@@ -39,6 +39,9 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
                 // 将 data 从 buffer 变成 string，然后放到 data 放到 response 里面
             });
             break;
+        default:
+            response.statusCode = 404;
+            response.end();
     }
 });
 
