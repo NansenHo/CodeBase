5c09651263b93e67d76139fbb9123c8dc01eb1b9
henansong <github@nansenho.com>
2021-05-24 09:37:31 +0800

    '匹配任何页面

--- Nodejs/node-server/url-response/index.ts
+++ Nodejs/node-server/url-response/index.ts
@@ -11,13 +11,18 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
     const {method, url: path, headers} = request;
     const {pathname, search} = url.parse(path);
 
-    const filename = pathname.substr(1); // 去掉 pathname 的 / 就得到了 filename
+    let filename = pathname.substr(1); // 去掉 pathname 的 / 就得到了 filename
+    if (filename === '') {
+        filename = 'index.html'
+    }
     fs.readFile(p.resolve(publicDir, filename), (error, data) => {
         if (error) {
-            // console.log(error);
-            if (error.errno === -4058) {
+            console.log(error);
+            if (error.errno === -4058 || error.errno === -2) {
                 response.statusCode = 404;
-                response.end('文件不存在');
+                fs.readFile(p.resolve(publicDir, '404.html'), (error, data) => {
+                    response.end(data);
+                })
             } else {
                 response.statusCode = 500;
                 response.end('服务器繁忙，请稍后重试');
